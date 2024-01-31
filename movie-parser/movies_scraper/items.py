8c0ff29eaf95449089360c03e1509b58840f421c
NovIrina <90452047+NovIrina@users.noreply.github.com>
2024-01-31 21:45:39 +0300

    added description and improved script

--- movie-parser/movies_scraper/items.py
+++ movie-parser/movies_scraper/items.py
@@ -3,10 +3,25 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from itemloaders.processors import Join, TakeFirst
+from scrapy.item import Field, Item
 
 
-class MoviesScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class MovieItem(Item):
+    """
+    The structure of the movie item for the scrapy spider.
+
+    Fields:
+        title: The title of the movie.
+        genre: The genre(s) of the movie.
+        director: The director(s) of the movie.
+        country: The country(ies) where the movie was produced.
+        year: The release year of the movie.
+        imdb_rating: The IMDb rating of the movie.
+    """
+    title = Field(output_processor=TakeFirst())
+    genre = Field(output_processor=Join(", "))
+    director = Field(output_processor=Join(", "))
+    country = Field(output_processor=Join(", "))
+    year = Field(output_processor=TakeFirst())
+    imdb_rating = Field(output_processor=TakeFirst())
--- movie-parser/movies_scraper/spiders/movies_spider.py
+++ movie-parser/movies_scraper/spiders/movies_spider.py
@@ -3,31 +3,12 @@ import json
 from typing import Any
 
 import scrapy
-from itemloaders.processors import Join, TakeFirst, MapCompose
+from itemloaders.processors import MapCompose
 from scrapy.crawler import CrawlerProcess
 from scrapy.http import Request, Response
-from scrapy.item import Field, Item
 from scrapy.loader import ItemLoader
 
-
-class MovieItem(Item):
-    """
-    The structure of the movie item for the scrapy spider.
-
-    Fields:
-        title: The title of the movie.
-        genre: The genre(s) of the movie.
-        director: The director(s) of the movie.
-        country: The country(ies) where the movie was produced.
-        year: The release year of the movie.
-        imdb_rating: The IMDb rating of the movie.
-    """
-    title = Field(output_processor=TakeFirst())
-    genre = Field(output_processor=Join(", "))
-    director = Field(output_processor=Join(", "))
-    country = Field(output_processor=Join(", "))
-    year = Field(output_processor=TakeFirst())
-    imdb_rating = Field(output_processor=TakeFirst())
+from movies_scraper.items import MovieItem
 
 
 def process_value(value: str) -> str | None:
